79c4c519bf2fb2c4947264f98dc71d9d6bad9261
Junghwan John Goh <jhgoh@cern.ch>
2015-04-23 17:24:22 +0900

    no detailed info, put TimeModule to messagelogger output

--- TimingPerf/python/perfCheck.py
+++ TimingPerf/python/perfCheck.py
@@ -3,16 +3,16 @@ import FWCore.ParameterSet.Config as cms
 def customisePerf(process):
     process.Timing = cms.Service("Timing")
     process.load("FWCore.MessageService.MessageLogger_cfi")
-    process.MessageLogger.destinations = cms.untracked.vstring("detailedInfo.log","cout")
+    process.MessageLogger.categories.append("TimeModule")
     process.options.wantSummary = cms.untracked.bool(True)
 
-    process.genMuons = cms.EDProducer("GenParticlePruner",
-        src = cms.InputTag("genParticles"),
-        select = cms.vstring('drop *', 
-            'keep abs(pdgId) == 13',
-            'drop pt <= 0', 
-        ),
-    )
+    #process.genMuons = cms.EDProducer("GenParticlePruner",
+    #    src = cms.InputTag("genParticles"),
+    #    select = cms.vstring('drop *',
+    #        'keep abs(pdgId) == 13',
+    #        'drop pt <= 0',
+    #    ),
+    #)
     #process.out.outputCommands.append('keep *_genMuons_*_*')
 
     return process
